344c0dd2a7fee7c70c7e97eddf0d0b7c0adce34f
Umair-Jamaat-Ali <umairansari758@gmail.com>
2023-04-03 15:35:45 +0500

    positive number

---
+++ script.js
@@ -0,0 +1,17 @@
+// write a program to check that number is negatives or positives
+
+function positiveOrNegative() {
+    var number = document.getElementById(number)
+    
+var pere = document.getElementById(pera);
+
+if(number=>0){
+    pera.innerHTML = "Number is Positive"
+}
+
+else{
+    pera.innerHTML = "Number is Negative"
+}
+
+
+}
